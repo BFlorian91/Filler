c6a111cf760c64bbcd5831ea4b3178f827103734
Florian <flbeaumo@student.42.fr>
2019-06-15 10:18:40 +0200

    parsing map ✅

--- srcs/get_datas.c
+++ srcs/get_datas.c
@@ -6,7 +6,7 @@
 /*   By: flbeaumo <flbeaumo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/13 19:36:56 by flbeaumo          #+#    #+#             */
-/*   Updated: 2019/06/14 16:08:30 by flbeaumo         ###   ########.fr       */
+/*   Updated: 2019/06/15 10:17:10 by flbeaumo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,27 +32,24 @@ int get_players(t_filler *datas)
 static int parsing_map(t_filler *datas)
 {
 	int 	i;
-	int		j;
 	int		x;
 	int 	y;
 	char	tmp[4096];
 
 	i = 0;
-	j = 0;
 	y = 0;
 	ft_strcpy(tmp, ft_strstr(datas->buffer, "000"));
-	if (!(datas->map = (char **)malloc(sizeof(char *) * (y + 1))))
+	if (!(datas->map = (char **)malloc(sizeof(char *) * (datas->map_height + 1))))
 		return (-1);
+	datas->map[15] = 0;
 	while (y < datas->map_height)
 	{
 		x = 0;
-		while (j < 5)
-			++j;
-		i += j;
-		j = 0;
-		if (!(datas->map[y] = (char *)malloc(sizeof(char) * (x + 1))))
+		if (!(datas->map[y] = (char *)malloc(sizeof(char) * (datas->map_width + 1))))
 				return (-1);
 		ft_bzero(datas->map[y], datas->map_width);
+		while (ft_isdigit(tmp[i]) || tmp[i] == ' ')
+			++i;
 		while (x < datas->map_width)
 		{
 			datas->map[y][x] = tmp[i];
@@ -62,7 +59,7 @@ static int parsing_map(t_filler *datas)
 		datas->map[y][x] = '\0';
 		++y;
 	}
-	datas->map[y] = 0;
+	
 	print_board(datas);
 	return (1);
 }
@@ -78,10 +75,10 @@ int	get_map(t_filler *datas)
 	ft_strcpy(tmp, ft_strstr(datas->buffer, "Plateau "));
 	while (!ft_isdigit(tmp[i]))
 		++i;
-	datas->map_width = get_number(tmp, &i);
+	datas->map_height = get_number(tmp, &i);
 	while (!ft_isdigit(tmp[i]))
 		++i;
-	datas->map_height = get_number(tmp, &i);
+	datas->map_width = get_number(tmp, &i);
 	parsing_map(datas);
 	return (1);
 }
